2275f7b28c77aedd384ad83245f21eabee69c740
matiq07 <136392093+matiq07@users.noreply.github.com>
2023-06-16 14:05:29 -0300

    Add files via upload

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,14 @@
+//Ejercicio 3 a)
+
+let fahrenheit = 50;
+let conversion_a_celsius= (fahrenheit - 32) * (5/9);
+console.log(conversion_a_celsius);
+
+// Ejercicio 3 b)
+fahrenheit= 75;
+conversion_a_celsius= (fahrenheit - 32) * (5/9);
+console.log(conversion_a_celsius);
+
+
+
+
---
+++ practico0/promedio3.js
@@ -0,0 +1,28 @@
+//Ejercicio 4
+//a)
+
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+let sumatoria = num1 + num2 + num3;
+let promedio = sumatoria/3;
+console.log(promedio);
+
+//b)
+num1= -2;
+num2 = -3;
+num3=-8;
+sumatoria= num1+ num2+ num3;
+promedio = sumatoria/3;
+console.log(promedio);
+
+//c)
+
+num1= -1.2;
+num2 = 5.3;
+num3=-3.3;
+sumatoria= num1+ num2+ num3;
+promedio = sumatoria/3;
+console.log(promedio);
+
+
