7c0521dfd95a7546604ea5d992bba16ecf26ef84
matiq07 <matiasjq2907@gmail.com>
2023-06-16 15:21:41 -0300

    agrega ejercicio1

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,11 @@
+//Ejercicio 1
+
+let nombre; //a) se define la variable y no se inicializa
+nombre= "Matias"; // b) inicializa la variable nombre
+let apellido= "Quispe"; //c) define la variable y se le asigna un valor
+let edad = 32; //d) mismo paso que c)
+console.log("Hola, " + nombre +" "+ apellido + ". Tienes" +" " + edad +" " +"años"); //e)
+
+
+
+
