d4bd52419ef5d55120abdf5303769243247c7352
matiq07 <matiasjq2907@gmail.com>
2023-06-17 23:36:58 -0300

    agrega ej 3

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,14 @@
+let num1= 3;
+let num2= 1;
+let num3= 21;
+let num4= 10;
+
+let sumaValor1Y2= num1+ num2;
+
+let restaValor3Y4= num3 - num4;
+
+let resultadoFinal= sumaValor1Y2 * restaValor3Y4;
+
+let esPar= (resultadoFinal % 2)===0
+
+console.log("mis variables iniciales fueron:" ,num1+",",num2+",",num3+" y",num4+".","La respuesta final a verificar si el resultado final es par es:", esPar)
