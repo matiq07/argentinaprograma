17003209bc864fd777a488f6ebb71d463ac6873b
matiq07 <matiasjq2907@gmail.com>
2023-06-22 19:45:43 -0300

    agrega ejercicio 5

---
+++ practico1/frutas.js
@@ -0,0 +1,23 @@
+// 4.a
+let frutas = [];
+
+//4.b
+
+frutas.push("naranja");
+frutas.push("pera");
+frutas.push("uva");
+frutas.push("manzana");
+frutas.push("frutilla");
+
+
+//4.c
+
+ultima=frutas.pop();
+console.log("La ultima fruta agregada fue:",ultima)
+
+//4.d
+reversoFrutas= frutas.reverse();
+primera= reversoFrutas.pop();
+
+console.log("La primera fruta agregada fue:",primera);
+
