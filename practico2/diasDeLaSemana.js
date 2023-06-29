daf0fe5d6d68a3160364cbae7c431d8caf654229
matiq07 <matiasjq2907@gmail.com>
2023-06-29 01:39:19 -0300

    ingresa ejercicio 4

---
+++ practico2/diasDeLaSemana.js
@@ -0,0 +1,28 @@
+
+switch(numero){
+    case 1:
+        console.log("Hoy es Lunes");
+        break;
+    case 2:
+        console.log("Hoy es Martes");
+        break;
+
+    case 3:
+        console.log("Hoy es Miercoles");
+        break;
+    case 4:
+        console.log("Hoy es Jueves");
+        break;
+    case 5:
+        console.log("Hoy es Viernes");
+        break;
+    case 6:
+        console.log("Hoy es Sabado");
+        break;
+
+    case 7:
+        console.log("Hoy es domingo");
+        break;
+    default:
+        console.log("Numero invalido");
+}
