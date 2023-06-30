37d1ccb913da8cec3ab162bf7b3eb8876a1dd359
matiq07 <matiasjq2907@gmail.com>
2023-06-30 01:51:01 -0300

    agrega ejercicio 1

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,13 @@
+let i= 10; //inicializador
+
+while(i>=0){
+    if(i===10){
+        console.log("cuenta regresiva 10");
+    }else if(i<=9 && i>=1){
+        console.log(i);
+    }else{
+        console.log("Lanzamiento");
+    }
+    i=i-1;   
+}
+
